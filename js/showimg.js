2108f6e6a22df295927d6d79ea628eb4065d8094
wingywing <wingy.wing@icloud.com>
2021-11-02 19:42:47 +1100

    ghost list

--- js/showimg.js
+++ js/showimg.js
@@ -1,11 +1,24 @@
+// var width = (window.innerWidth > 0) ? window.innerWidth : document.documentElement.clientWidth;
+// $(document).ready(function(){
+//     if (width > 992) {
+//         $('.projLink').mouseover(function() {
+//             myvar = this.id;
+//             $(".projectImg").fadeOut("50", "swing");
+//              $('#div'+myvar).fadeIn("100", "swing");
+             
+//       });
+//     }
+// }); To fade in and out from nothing
+
+
+//To fade to 100 opacity from 20% on hover
 var width = (window.innerWidth > 0) ? window.innerWidth : document.documentElement.clientWidth;
 $(document).ready(function(){
     if (width > 992) {
         $('.projLink').mouseover(function() {
             myvar = this.id;
-            $(".projectImg").fadeOut("50", "swing");
-             $('#div'+myvar).fadeIn("100", "swing");
-             
+            $(".projectImg").fadeTo("fast", .20);
+            $('#div'+myvar).fadeTo("fast", 1.0);
       });
     }
 });
